f50acfcfc6e8ec80f25d5a033d83b91e16dd6e28
Bjarni Fridjonsson <bjarni_petur@hotmail.com>
2019-06-12 11:54:11 +0200

    Gjallarhorn - Created offline installer for QV 2.0.6

--- Code/Tools/Gjallarhorn/Installer/CreateOutputs.bat
+++ Code/Tools/Gjallarhorn/Installer/CreateOutputs.bat
@@ -1,12 +1,21 @@
 del "%~dp0\output\Qlik Proactive Express For QlikView Installer.exe"
+del "%~dp0\output\Qlik Proactive Express For QlikView Offline Installer.exe"
 del "%~dp0\output\Qlik Proactive Express For Qlik Sense Installer.exe"
 del "%~dp0\output\Qlik Proactive Express For Qlik Sense Offline Installer.exe"
 
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"  C:\src\GitHub\Qlik-SupportTools\Code\Tools\Gjallarhorn\Gjallarhorn.sln /t:Build /p:Configuration=Release 
+if errorlevel 1 Goto errorHandling
 
 del "%~dp0..\Gjallarhorn\bin\AnyCPU\Release\Settings.xml"
 ren "%~dp0..\Gjallarhorn\bin\AnyCPU\Release\SettingsProdQv.xml" Settings.xml
 "C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc "%~dp0\InstallerScript.iss"
 ren "%~dp0\output\Qlik Proactive Express Installer.exe" "Qlik Proactive Express For QlikView Installer.exe"
+
+del "%~dp0..\Gjallarhorn\bin\AnyCPU\Release\Settings.xml"
+ren "%~dp0..\Gjallarhorn\bin\AnyCPU\Release\SettingsProdQvOffline.xml" Settings.xml
+"C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc "%~dp0\InstallerScript.iss"
+ren "%~dp0\output\Qlik Proactive Express Installer.exe" "Qlik Proactive Express For QlikView Offline Installer.exe"
+
 
 del "%~dp0..\Gjallarhorn\bin\AnyCPU\Release\Settings.xml"
 ren "%~dp0..\Gjallarhorn\bin\AnyCPU\Release\SettingsProdSense.xml" Settings.xml
@@ -18,4 +27,10 @@ ren "%~dp0..\Gjallarhorn\bin\AnyCPU\Release\SettingsProdSenseOffline.xml" Settin
 "C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc "%~dp0\InstallerScript.iss"
 ren "%~dp0\output\Qlik Proactive Express Installer.exe" "Qlik Proactive Express For Qlik Sense Offline Installer.exe"
 
+
 start explorer.exe "%~dp0output\"
+GOTO:EOF
+
+:errorHandling
+   echo Failure Reason Given is %errorlevel%
+   exit /b %errorlevel%

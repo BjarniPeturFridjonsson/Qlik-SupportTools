7a4de8f9ecfe385911e6957d0108c10aaf8a4422
Bjarni Fridjonsson <bjarni_petur@hotmail.com>
2019-06-18 11:13:13 +0200

    Gjallarhorn - Test data generator added for end to end testing capabilities.

--- Code/Tools/Gjallarhorn/TestDataGenerator/DoWork.cs
+++ Code/Tools/Gjallarhorn/TestDataGenerator/DoWork.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Eir.Common.IO;
+using FreyrCommon.Models;
 using Gjallarhorn.Db;
 using Gjallarhorn.Monitors.QmsApi;
 using Gjallarhorn.SenseLogReading.FileMiners;
@@ -15,30 +13,54 @@ namespace TestDataGenerator
     {
         public void GetItDone()
         {
+            var txtData = new TextData();
+
+            var installationId = "f113051f-ef10-4969-a2b1-e3c890506b4e";
             var monitorNameFile = "SenseLogFileParserMonitor";
+            var monitorNameStats = "SenseStatisticsMonitor";
             var date = DateTime.Now.AddMonths(-12);
             var gjallarhornDb = new GjallarhornDb(FileSystem.Singleton);
             gjallarhornDb.EnsureMonitorTableExists(monitorNameFile);
+            gjallarhornDb.EnsureMonitorTableExists(monitorNameStats);
             Random random = new Random();
+            
+
             while (date < DateTime.Now)
             {
                 date = date.AddHours(1);
-               
+
+                var dto = new FileMinerDto();
+                dto.LicenseSerialNo = "(fake LICENCE)";
+                dto.CollectionDateUtc = date;
+                dto.IsMonthly = false;
+
+                var data = new StatisticsDto { LogFileMinerData = dto, CollectionDateUtc = dto.CollectionDateUtc };
+                data.QLikSenseCalInfo = JsonConvert.DeserializeObject<QLikSenseCalInfo>(txtData.GetCalInfo());
+                data.InstallationId = "(fake)_" + installationId;
+                dto.TotalUniqueActiveAppsList = new Dictionary<string, int>();
+                gjallarhornDb.SaveMonitorData(monitorNameFile, JsonConvert.SerializeObject(data));
+
+                
                 if (date.Hour == 0)
                 {
-                    var dto = new FileMinerDto();
-                    dto.LicenseSerialNo = "(fake LICENCE)";
-                    dto.CollectionDateUtc = date;
-                    dto.IsMonthly = false;
+                    var dtoDay = new FileMinerDto();
+                    dtoDay.LicenseSerialNo = "(fake)";
+                    dtoDay.CollectionDateUtc = date;
                     dto.TotalUniqueActiveApps = random.Next(100);
                     dto.TotalUniqueActiveUsers = random.Next(55);
                     dto.SessionLengthAvgInMinutes = random.Next(100);
                     dto.SessionLengthMedInMinutes = random.Next(50);
 
-                    var data = new StatisticsDto { LogFileMinerData = dto, CollectionDateUtc = dto.CollectionDateUtc };
-                    data.InstallationId = "(fake)_" + Guid.NewGuid();
-                    dto.TotalUniqueActiveAppsList = new Dictionary<string, int>();
-                    gjallarhornDb.SaveMonitorData(monitorNameFile, JsonConvert.SerializeObject(data));
+                    var dataDay = new StatisticsDto { LogFileMinerData = dtoDay, CollectionDateUtc = dtoDay.CollectionDateUtc };
+
+                    dataDay.InstallationId = "(fake)_" + installationId;
+                    dtoDay.TotalUniqueActiveAppsList = new Dictionary<string, int>();
+                    dataDay.QlikSenseMachineInfos = JsonConvert.DeserializeObject<List<QlikSenseMachineInfo>>(txtData.GetMachineInfo());
+                    dataDay.QlikSenseServiceInfo = JsonConvert.DeserializeObject<List<QlikSenseServiceInfo>>(txtData.GetServiceInfo());
+                    dataDay.QlikSenseQrsAbout = JsonConvert.DeserializeObject<QlikSenseQrsAbout>(txtData.GetQrsAbout());
+                    dataDay.QlikSenseAppListShort = JsonConvert.DeserializeObject<List<QlikSenseAppListShort>>(txtData.GetAppList());
+
+                    gjallarhornDb.SaveMonitorData(monitorNameStats, JsonConvert.SerializeObject(dataDay));
                 }
             }
         }
